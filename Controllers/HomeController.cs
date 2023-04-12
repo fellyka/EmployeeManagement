b8cc371d1f9f6b20f7ae1fe2abc81e47c1e3a5b2
Felly KANYIKI <fellyka@gmail.com>
2023-04-12 21:07:46 +0200

    Passing Data from the Controller to the View using ViewBag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,8 +25,8 @@ namespace EmployeeManagement.Controllers
         public IActionResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            ViewData["Employee"] = model;
-            ViewData["PageTitle"] = "Employee Details";
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "Employee Details";
             return View(model);
         }
     }
