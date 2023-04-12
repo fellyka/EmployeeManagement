61fa16f67d068b1b762b0a054a32d47983a5791f
Felly KANYIKI <fellyka@gmail.com>
2023-04-12 16:58:11 +0200

    Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +8,7 @@ using System.Linq;
 
 namespace EmployeeManagement.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
 
@@ -18,6 +20,12 @@ namespace EmployeeManagement.Controllers
         {
             var employee = _employeeRepository.GetEmployee(1).Name;
             return employee;
+        }
+
+        public IActionResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return Json(model);
         }
     }
 }
