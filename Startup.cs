ad9d47f87fda9bfce70ec6c84f23a5600cbc926a
Felly KANYIKI <fellyka@gmail.com>
2023-04-14 15:51:18 +0200

    Creating a connection in the startup.cs class and configuring a connection string in the appsettings.json file

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,9 +25,13 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options =>
+            options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+
             services.AddMvc().AddXmlSerializerFormatters();
-            services.AddSingleton<IEmployeeRepository,MockEmployeeRepository>();
-           // services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
+            //services.AddSingleton<IEmployeeRepository,MockEmployeeRepository>();
+            // services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
