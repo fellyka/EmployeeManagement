3258f59faebc9da2db47c055a7b1a67edb63bd3b
Felly KANYIKI <fellyka@gmail.com>
2023-04-19 14:15:44 +0200

    New User Registration

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] /* To Mask characters */
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password",ErrorMessage = "Password and confirmation Password do not match\nPlease Try again")]
+        public string ConfirmPassword { get; set;}
+    }
+}
