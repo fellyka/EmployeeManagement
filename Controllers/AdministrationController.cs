ec6f887ab2824a6ac04e7d9de97d4e7647c528d9
Felly KANYIKI <fellyka@gmail.com>
2023-04-26 13:10:20 +0200

    Creating Roles

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,54 @@
+﻿using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole( CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                //Create Role in tthe Database
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "home");
+                }
+
+                //Should errors occur
+
+                foreach(IdentityError error in result.Errors)
+                {
+                   ModelState.AddModelError("",error.Description);
+                }
+
+            }
+           
+            return View(model);
+        }
+    }
+
+}
---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
