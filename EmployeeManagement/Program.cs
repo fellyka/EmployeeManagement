fde969c047e8f5b86e97daf995bfc1be50441347
Felly Kanyiki <fellyka@gmail.com>
2025-02-16 19:13:51 +0200

    Commenting the Main file(Program.cs) class and updating the port number to 5000

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,6 +1,11 @@
+//This line creates a WebApplicationBuilder instance used to configure the web application.
+//It sets ups the environment for building the web application
 var builder = WebApplication.CreateBuilder(args);
+
+// This line builds the web application using the configuration provided by the builder
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// This line starts the web application and begins listening for incoming requests.
 app.Run();
