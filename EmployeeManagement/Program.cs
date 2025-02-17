7a1182edcfa9af827e2e6c4234156b9a583e7eff
Felly Kanyiki <fellyka@gmail.com>
2025-02-17 21:23:51 +0200

    wwwroot folder added

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,9 +1,31 @@
 //This line creates a WebApplicationBuilder instance used to configure the web application.
 //It sets ups the environment for building the web application
+using System.Runtime.Intrinsics.X86;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// This line adds MVC services to the dependency injection container.
+builder.Services.AddControllersWithViews();
 
 // This line builds the web application using the configuration provided by the builder
 var app = builder.Build();
+
+//Configure the HTTP request pipeline
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+// Serve static files from the wwwroot folder
+app.UseStaticFiles();
+
+
+// Map endpoints to controllers
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.MapGet("/", () => "Hello World!");
 
